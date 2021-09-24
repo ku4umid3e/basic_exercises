73d871f70233b5273919d021a8af5fb65fea1c8d
ku4umid3e <kuchumid3e@gmail.com>
2021-09-24 11:24:48 +0300

    ДЗ со строками готово

--- string_challenges.py
+++ string_challenges.py
@@ -19,9 +19,14 @@ print(len(sentence.split(' ')))
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+#print(*[i[0] + '\n' for i in sentence.split(' ')])
+for word in sentence.split(' '):
+    print(word[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+len_word = []
+for word in sentence.split(' '):
+    len_word.append(len(word.strip()))
+print(int(sum(len_word) / len(len_word)))
